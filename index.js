06f702fa36dd9ceac962006c02692c3fc8ab8c9e
Sigifredo <93165649+Yumewak@users.noreply.github.com>
2021-11-12 10:36:14 -0800

    Update index.js

--- index.js
+++ index.js
@@ -35,3 +35,19 @@ if (randomNumber1 > randomNumber2) {
     document.querySelector("h1").innerHTML = "Empate";
 } 
 }
+
+/*
+function dados() {
+    var randomNumber1 = Math.floor((Math.random()*6)+1);
+    document.querySelectorAll("img").setAttribute("src", "images/dice" + randomNumber1 + ".png")
+    var randomNumber2 = Math.floor((Math.random()*6)+1);
+    document.querySelectorAll("img")[1].setAttribute("src", "images/dice" + randomNumber2 + ".png")
+if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "Gano Alfredo";
+}if (randomNumber1 < randomNumber2) {
+    document.querySelector("h1").innerHTML = "Gano Salvatore";
+}if (randomNumber1 === randomNumber2) {
+    document.querySelector("h1").innerHTML = "Empate";
+} 
+}
+*/
